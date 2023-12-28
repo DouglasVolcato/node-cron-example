d68dca180c503286f525750e7a65c52b74ab1267
DouglasVolcato <douglasvolcato@gmail.com>
2023-12-28 19:51:45 -0300

    chore: implement default home route message

--- src/controllers/home-controller.ts
+++ src/controllers/home-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export default function HomeController(req: Request, res: Response) {
-  return res.json({ message: "Confirmation message!" });
+  return res.json({ message: "App working!" });
 }
